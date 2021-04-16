94f6fdf5df5f924b73606cbb383563ff3bf99ebc
Tommy Li <ttoo74@gmail.com>
2021-04-16 14:28:56 +0800

    temp navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './home/Home'
 import Search from './search/Search'
+import Navbar from './Navbar'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/search">
           <Search />
